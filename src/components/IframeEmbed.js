4eaa08c2ca0e33783bf737f9788626a3475ea55b
vaspoz <vaspoz85@gmail.com>
2024-05-24 20:45:52 +0200

    IFrame fix

--- src/components/IframeEmbed.js
+++ src/components/IframeEmbed.js
@@ -6,7 +6,7 @@ export default function IframeEmbed({ postfix }) {
       src={`https://archive.0xcafe.news/${postfix}`}
       allow
       allowFullScreen
-      sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation"
+      sandbox
     />
   );
 }
