1d9739754919389709d12e04dabda168960f0978
vaspoz <vaspoz85@gmail.com>
2024-08-15 12:39:17 +0200

    Add hotjar into static scripts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,8 +17,11 @@ const config: Config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  clientModules: ["./src/headScripts/hotjar.js"],
-  
+  scripts: [
+    {
+      src: "/js/hotjar.js"
+    },
+  ],
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
