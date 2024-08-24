acd00e716270d8c83b010ae5a496591e1e1f166b
vaspoz <vaspoz@users.noreply.github.com>
2024-08-24 10:42:30 +0200

    feat(docusaurus.config.ts): add "Buy Me a Coffee" button to the header for user support and engagement

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,6 +79,11 @@ const config: Config = {
           href: "/docs/privacy",
         },
         { to: "/blog", label: "Archive", position: "right" },
+        {
+          type: 'html',
+          position: 'right',
+          value: `<a href='https://ko-fi.com/R5R6Q8N0Z' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi1.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>`,
+        },
       ],
     },
     footer: {
