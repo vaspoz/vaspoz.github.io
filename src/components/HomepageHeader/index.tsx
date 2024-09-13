dc2ccc16e593b43bc4083a9e55b96eb9402753c3
vaspoz <vaspoz85@gmail.com>
2024-09-13 18:07:22 +0200

    test

---
+++ src/components/HomepageHeader/index.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useRef } from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Form from "@site/src/components/HomepageSubscribe";
+import { NeatGradient } from "@firecms/neat";
+import { config } from "@site/static/js/NeatConfig";
+import classNames from "classnames";
+
+import CodeSection from "@site/src/components/CodeSection";
+import styles from "./styles.module.css";
+
+type SystemWindowProps = JSX.IntrinsicElements["div"];
+
+function SystemWindow({ children, className, ...props }: SystemWindowProps) {
+  return (
+    <div
+      {...props}
+      className={classNames(
+        styles["system-window"],
+        styles["blue-accent"],
+        styles["preview-border"],
+        className,
+      )}
+    >
+      <div className={styles["system-top-bar"]}>
+        <span
+          className={styles["system-top-bar-circle"]}
+          style={{ backgroundColor: "#8759ff" }}
+        />
+        <span
+          className={styles["system-top-bar-circle"]}
+          style={{ backgroundColor: "#3fc4fe" }}
+        />
+        <span
+          className={styles["system-top-bar-circle"]}
+          style={{ backgroundColor: "#42ffac" }}
+        />
+      </div>
+      {children}
+    </div>
+  );
+}
+
+function Showcase() {
+  const codeSnippet = `
+// <schema>
+let newsletterDescription = {
+  name: "0xCAFE",
+  frequency: "weekday",
+  sections: [
+    "Tech and Science news",
+    "Articles",
+    "Postmortems",
+    "App of the day",
+    "GitHub repositories",
+    "Puzzles",
+  ],
+  audience: {
+    type: [
+      "software developers",
+      "engineers",
+      "architects",
+      "tech enthusiasts",
+    ],
+    currentSubscribers: 1500,
+  },
+  stats: {
+    issuesSentBase: 400,
+    issuesSendModificator: "moreThan",
+    isFree: true,
+    canUnsubscribe: true,
+  },
+};
+// </schema>
+  `;
+  return (
+    <SystemWindow>
+      <CodeSection language="js" section="schema" source={codeSnippet} />
+    </SystemWindow>
+  );
+}
+
+export default function HomepageHeader(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  const gradientRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (gradientRef.current) {
+      console.log(gradientRef.current);
+      new NeatGradient({
+        ref: gradientRef.current,
+        ...config,
+      });
+    }
+  }, []);
+
+  return (
+    <div className={clsx("hero hero--primary", styles.heroBanner)}>
+      <canvas
+        id="gradient"
+        ref={gradientRef}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+        }}
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <span
+              className={classNames(
+                styles.text,
+                styles["text-big"],
+                styles.title,
+              )}
+            >
+              {siteConfig.title}
+            </span>
+            <h1
+              className={classNames(
+                styles.description,
+                styles.text,
+                styles["text-huge"],
+              )}
+            >
+              The only newsletter you need to stay updated with latest
+            </h1>
+            <ul className={classNames(styles.bullets)}>
+              <li>📰 News</li>
+              <li>📚 Articles</li>
+              <li>☠️ Postmortems</li>
+              <li>🚀 App of the day</li>
+              <li>👨‍💻 GitHub repositories</li>
+            </ul>
+            <Form />
+          </div>
+          <div className="col">
+            <Showcase />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Quote/index.tsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+export default function QuotesSection() {
+  return (
+    <div className={clsx("container", styles.feature)}>
+      <div className="row">
+        <div className="col">
+          <div className="avatar avatar--vertical margin-bottom--sm">
+            <img
+              alt="Basil Pozdeev"
+              className="avatar__photo avatar__photo--xl"
+              src={"https://avatars.githubusercontent.com/u/4658186?v=4"}
+              style={{ overflow: "hidden" }}
+            />
+            <div className="avatar__intro padding-top--sm">
+              <div className="avatar__name">Basil Pozdeev</div>
+              <small className="avatar__subtitle">
+                The guy who actually thought this was a good idea
+              </small>
+            </div>
+          </div>
+          <p className={clsx("text--center", styles.quote)}>
+            <p>
+              I’ve been in the IT industry for years, and like many of you,
+              being an IT engineer means never stopping learning.
+              <br />
+              Reading articles, news, and doomscrolling GitHub repositories
+              became a daily routine.
+              <br />
+              Then adulthood hit and I got a bit lazy. So, what does an IT guy
+              do? Automate, of course! I built a CLI tool to quickly
+              <br />
+              grab all the morning info I needed. Eventually, I decided to share
+              it and dive into the newsletter world.
+              <br />
+              <br />
+              Now, here we are—over a year, 400+ issues, and a thriving website.
+              <br />A huge thank you to all my subscribers; your support keeps
+              me energized to grow and create new content! ❤️
+            </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
