6b4a387c17ba92ec9b1646f12c5b6819d7337856
Basil Pozdeev <vaspoz@users.noreply.github.com>
2025-07-19 23:24:27 +0200

    moved to main folder

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/basil/repos/vaspoz.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/basil/repos/vaspoz.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/basil/repos/vaspoz.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/basil/repos/vaspoz.github.io/src/css/custom.css"),
+];
