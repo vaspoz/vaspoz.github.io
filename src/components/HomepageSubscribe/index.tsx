c391b27ec186d07e9768b954eb6330e3ec55baac
Basil Pozdeev <vaspoz@users.noreply.github.com>
2025-12-03 09:54:35 +0100

    fix: rollback

--- src/components/HomepageSubscribe/index.tsx
+++ src/components/HomepageSubscribe/index.tsx
@@ -3,7 +3,7 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 import React from "react";
 
 const url =
-  "https://news.us21.list-manage.com/subscribe/post?u=REDACTED_MAILCHIMP_U&amp;id=REDACTED_MAILCHIMP_ID_0&amp;f_id=REDACTED_MAILCHIMP_FID";
+  "https://news.us21.list-manage.com/subscribe/post?u=REDACTED_MAILCHIMP_U&amp;id=REDACTED_MAILCHIMP_ID&amp;f_id=REDACTED_MAILCHIMP_FID";
 
 const CustomForm = ({ status, message, onValidated }) => {
   let email;
